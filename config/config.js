35f182df179d89b84b7a99b685a95eaf41a15c40
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 00:03:49 +0800

    chore(config): add steinStore to config
    Add steinStore to config exports for easy access
    
    Chore

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
 require("dotenv-safe").config({
   allowEmptyValues: false,
 });
+const SteinStore = require("stein-js-client");
 
 module.exports.STORAGE_URL = process.env.STORAGE_URL;
 module.exports.FISH_PRICE_URL = process.env.FISH_PRICE_URL;
 module.exports.OPTION_AREA_URL = process.env.OPTION_AREA_URL;
 module.exports.OPTION_SIZE_URL = process.env.OPTION_SIZE_URL;
+module.exports.steinStore = new SteinStore(process.env.STORAGE_URL);
