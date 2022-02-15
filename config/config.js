be18beaf0e72b9f114ce70fd87fa46a5f25b7406
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 07:40:26 +0800

    chore(config.js): add STORAGE_TEST_URL
    Add STORAGE_TEST_URL to used as a test api access
    
    Chore

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ require("dotenv-safe").config({
 const SteinStore = require("stein-js-client");
 
 module.exports.STORAGE_URL = process.env.STORAGE_URL;
+module.exports.STORAGE_TEST_URL = process.env.STORAGE_TEST_URL;
 module.exports.FISH_PRICE_URL = process.env.FISH_PRICE_URL;
 module.exports.OPTION_AREA_URL = process.env.OPTION_AREA_URL;
 module.exports.OPTION_SIZE_URL = process.env.OPTION_SIZE_URL;
