9940ad5bef9d69b3d5c85c680d753cc0468cd4a9
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 18:52:50 +0800

    refactor(utils): add more check dataIsEmpty utils
    Add more check with string null, undefined and type object and date
    
    Refactor

--- src/utils/dataIsEmpty.js
+++ src/utils/dataIsEmpty.js
@@ -1,5 +1,20 @@
 module.exports = function dataIsEmpty(data) {
-  if (data === undefined || data === null || data === "") {
+  if (
+    data === undefined ||
+    data === null ||
+    data === "" ||
+    data === "null" ||
+    data === "undefined"
+  ) {
+    return true;
+  }
+  if (typeof data === "object" && Object.keys(data).length === 0) {
+    if (new Date(data).toString() !== "Invalid Date") {
+      return false;
+    }
+    return true;
+  }
+  if (Array.isArray(data) && data.length === 0) {
     return true;
   }
   return false;
