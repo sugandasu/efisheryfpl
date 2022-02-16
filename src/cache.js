4a0c4b7dffac54e1003dd88a6acf01c892c944df
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 11:41:50 +0800

    feat(cache): create cache class
    Create cache class to manage cache interaction using nodeCache package
    
    Feat

---
+++ src/cache.js
@@ -0,0 +1,47 @@
+const NodeCache = require("node-cache");
+
+class cache {
+  constructor(ttlSeconds = 60 * 60 * 1) {
+    this.cache = new NodeCache({
+      stdTTL: ttlSeconds,
+      checkperiod: ttlSeconds * 0.5,
+      useClones: false,
+    });
+  }
+
+  get(key, callbackFunction) {
+    const value = this.cache.get(key);
+
+    if (value) {
+      return Promise.resolve(value);
+    }
+
+    return callbackFunction().then((result) => {
+      this.cache.set(key, result);
+      return result;
+    });
+  }
+
+  del(keys) {
+    this.cache.del(keys);
+  }
+
+  delStartWith(startStr = "") {
+    if (!startStr) {
+      return;
+    }
+
+    const keys = this.cache.keys();
+    for (const key of keys) {
+      if (key.indexOf(startStr) === 0) {
+        this.del(key);
+      }
+    }
+  }
+
+  flush() {
+    this.cache.flushAll();
+  }
+}
+
+module.exports = cache;
