8ab05223bbe5d850e8766e0adf5b3e5ad8a27ad7
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 19:36:14 +0800

    refactor(getAllByRange): change return value
    Change getAllByRange to match return  data with searchData return
    
    Refactor

--- src/lib/getAllByRange.js
+++ src/lib/getAllByRange.js
@@ -7,52 +7,60 @@ const parseToDate = require("../utils/parseToDate");
 
 module.exports = function getAllByRange(harga, size, tanggal) {
   return new Promise(async (resolve, reject) => {
-    const sheetName = FISH_PRICE_SHEETNAME;
-    const ranges = {};
-    if (!dataIsEmpty(harga) && dataIsRange(harga)) {
-      if (dataIsRange(harga)) {
-        ranges.harga = { min: harga.min, max: harga.max };
-      }
-    }
-
-    if (!dataIsEmpty(size) && dataIsRange(size)) {
-      ranges.size = { min: size.min, max: size.max };
-    }
-
-    if (!dataIsEmpty(tanggal) && dataIsRange(tanggal)) {
-      ranges.tanggal = { min: tanggal.min, max: tanggal.max };
-    }
-
-    const resultSearchData = await searchData(sheetName, {});
-    if (resultSearchData.success !== true) {
-      return resolve(resultSearchData);
-    }
-
-    const resultSearchDataFilter = resultSearchData.data.filter((item) => {
-      let passed = !dataIsEmpty(item);
-
-      if (passed) {
-        if (ranges.harga) {
-          if (!dataIsInRange("number", ranges.harga, parseFloat(item.price)))
-            return false;
-        }
-
-        if (ranges.size) {
-          if (!dataIsInRange("number", ranges.size, parseFloat(item.size)))
-            return false;
-        }
-
-        if (ranges.tanggal) {
-          if (
-            !dataIsInRange("date", ranges.tanggal, parseToDate(item.tgl_parsed))
-          )
-            return false;
+    try {
+      const sheetName = FISH_PRICE_SHEETNAME;
+      const ranges = {};
+      if (!dataIsEmpty(harga) && dataIsRange(harga)) {
+        if (dataIsRange(harga)) {
+          ranges.harga = { min: harga.min, max: harga.max };
         }
       }
 
-      return passed;
-    });
+      if (!dataIsEmpty(size) && dataIsRange(size)) {
+        ranges.size = { min: size.min, max: size.max };
+      }
 
-    resolve(resultSearchDataFilter);
+      if (!dataIsEmpty(tanggal) && dataIsRange(tanggal)) {
+        ranges.tanggal = { min: tanggal.min, max: tanggal.max };
+      }
+
+      const resultSearchData = await searchData(sheetName, {});
+      if (resultSearchData.success !== true) {
+        return resolve(resultSearchData);
+      }
+
+      const resultSearchDataFilter = resultSearchData.data.filter((item) => {
+        let passed = !dataIsEmpty(item);
+
+        if (passed) {
+          if (ranges.harga) {
+            if (!dataIsInRange("number", ranges.harga, parseFloat(item.price)))
+              return false;
+          }
+
+          if (ranges.size) {
+            if (!dataIsInRange("number", ranges.size, parseFloat(item.size)))
+              return false;
+          }
+
+          if (ranges.tanggal) {
+            if (
+              !dataIsInRange(
+                "date",
+                ranges.tanggal,
+                parseToDate(item.tgl_parsed)
+              )
+            )
+              return false;
+          }
+        }
+
+        return passed;
+      });
+
+      resolve({ success: true, data: resultSearchDataFilter });
+    } catch (error) {
+      resolve({ success: false, data: error });
+    }
   });
 };
