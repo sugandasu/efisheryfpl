b137c381544f81a9279ccab50b4d62a5afae2ac2
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 16:18:18 +0800

    refactor(config): Change config .env variable
    Change URL to SHEETNAME
    
    Refactor

--- config/config.js
+++ config/config.js
@@ -5,6 +5,6 @@ require("dotenv-safe").config({
 module.exports.NODE_ENV = process.env.NODE_ENV || "production";
 module.exports.STORAGE_URL = process.env.STORAGE_URL;
 module.exports.STORAGE_TEST_URL = process.env.STORAGE_TEST_URL;
-module.exports.FISH_PRICE_URL = process.env.FISH_PRICE_URL;
-module.exports.OPTION_AREA_URL = process.env.OPTION_AREA_URL;
-module.exports.OPTION_SIZE_URL = process.env.OPTION_SIZE_URL;
+module.exports.FISH_PRICE_SHEETNAME = process.env.FISH_PRICE_SHEETNAME;
+module.exports.OPTION_AREA_SHEETNAME = process.env.OPTION_AREA_SHEETNAME;
+module.exports.OPTION_SIZE_SHEETNAME = process.env.OPTION_SIZE_SHEETNAME;
