95c1c83462b609e3386953c206f5ce41beca6b8e
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 22:13:00 +0800

    feat(deleteRecords): create deleteRecords
    Create deleteRecords wrapper to delete fish prices by id
    
    Feat

---
+++ src/lib/deleteRecords.js
@@ -0,0 +1,23 @@
+const { FISH_PRICE_SHEETNAME } = require("../../config/config");
+const dataIsEmpty = require("../utils/dataIsEmpty");
+const deleteData = require("./deleteData");
+
+module.exports = function deleteRecords(id) {
+  return new Promise(async (resolve, _) => {
+    try {
+      const sheetName = FISH_PRICE_SHEETNAME;
+      if (dataIsEmpty(id)) {
+        return resolve({ success: false, data: "Id tidak boleh kosong" });
+      }
+
+      const resultDeleteData = await deleteData(sheetName, { uuid: id });
+      if (!resultDeleteData.success) {
+        return resolve(resultDeleteData);
+      }
+
+      return resolve({ success: true, data: resultDeleteData.data });
+    } catch (err) {
+      return resolve({ success: false, data: err });
+    }
+  });
+};
