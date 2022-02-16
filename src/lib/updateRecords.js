324c2a0a3a29f7be37db77f9b554daff352cef05
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 22:03:37 +0800

    feat(updateRecords): create updateRecords
    Create updateRecords wrapper to update  fish prices if data set exists
    
    Feat

---
+++ src/lib/updateRecords.js
@@ -0,0 +1,45 @@
+const { FISH_PRICE_SHEETNAME } = require("../../config/config");
+const dataIsEmpty = require("../utils/dataIsEmpty");
+const updateData = require("./updateData");
+const getById = require("./getById");
+const validateOptionalFishPrice = require("../validation/validateOptionalFishPrice");
+
+module.exports = function updateRecord(id, set) {
+  return new Promise(async (resolve, _) => {
+    try {
+      const sheetName = FISH_PRICE_SHEETNAME;
+      if (dataIsEmpty(id)) {
+        return resolve({ success: false, data: "Id tidak boleh kosong" });
+      }
+      if (dataIsEmpty(set)) {
+        resolve({ success: false, data: "Data set tidak boleh kosong" });
+      }
+
+      const { newSet, errors } = validateOptionalFishPrice(set);
+
+      if (Object.keys(errors).length > 0) {
+        resolve({ success: false, data: errors });
+      }
+
+      if (dataIsEmpty(newSet)) {
+        resolve({ success: false, data: "Data set tidak boleh kosong" });
+      }
+
+      const resultUpdateData = await updateData(
+        sheetName,
+        { uuid: id },
+        newSet
+      );
+
+      if (!resultUpdateData.success) {
+        return resolve(resultUpdateData);
+      }
+
+      const resultGetById = await getById(id);
+
+      return resolve({ success: true, data: resultGetById.data });
+    } catch (err) {
+      return resolve({ success: false, data: err });
+    }
+  });
+};
