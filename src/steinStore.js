463f0c2786f77731021a2121d8a00e95a5b68b14
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 12:23:55 +0800

    refactor(steinStore): return default steinStore
    Return data from variable steinStore to default
    
    Refactor

--- src/steinStore.js
+++ src/steinStore.js
@@ -1,6 +1,6 @@
 const { NODE_ENV, STORAGE_URL, STORAGE_TEST_URL } = require("../config/config");
 const SteinStore = require("stein-js-client");
 
-module.exports.steinStore = new SteinStore(
+module.exports = new SteinStore(
   NODE_ENV === "production" ? STORAGE_URL : STORAGE_TEST_URL
 );
