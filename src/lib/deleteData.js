bab5eb9b179c5dc9e3bf36dd8aba7fc1de18d14e
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-16 15:20:09 +0800

    feat(deleteData): create deleteData
    Create  wraper to handle delete data on stein package and api
    
    Feat

---
+++ src/lib/deleteData.js
@@ -0,0 +1,38 @@
+const steinStore = require("../steinStore");
+const convertToReadableSteinError = require("../utils/convertToReadableSteinError");
+const checkSuccessResponse = require("../utils/checkSuccessResponse");
+
+module.exports = function deleteData(sheetName, search, limit) {
+  return new Promise(async (resolve, _) => {
+    const options = {};
+    if (typeof sheetName !== "string") {
+      sheetName = JSON.stringify(sheetName);
+    }
+    if (typeof search === "object") {
+      if (Object.keys(search).length === 0) {
+        return resolve({
+          success: false,
+          data: "Data search tidak boleh kosong",
+        });
+      }
+      options.search = search;
+    } else {
+      return resolve({
+        success: false,
+        data: "Format data search harus berupa object",
+      });
+    }
+    if (typeof limit === "number") {
+      options.limit = limit;
+    }
+
+    await steinStore
+      .delete(sheetName, options)
+      .then((res) => {
+        return resolve(checkSuccessResponse(res));
+      })
+      .catch((err) => {
+        return resolve(convertToReadableSteinError(err));
+      });
+  });
+};
