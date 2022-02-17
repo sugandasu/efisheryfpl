54a0e52c0a4220314a052a0cd2a6622eacbacbe7
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-17 17:57:27 +0800

    feat(getAll): create getAll library
    Create getAll library to handle search data to fish price with options

---
+++ src/lib/getAll.js
@@ -0,0 +1,24 @@
+const { FISH_PRICE_SHEETNAME } = require("../../config/config");
+const searchData = require("./searchData");
+
+module.exports = function getAll(options) {
+  return new Promise(async (resolve, _) => {
+    try {
+      const sheetName = FISH_PRICE_SHEETNAME;
+      const { search, limit, offset } = options;
+      const resultSearchData = await searchData(
+        sheetName,
+        search,
+        limit,
+        offset
+      );
+      if (resultSearchData.success !== true) {
+        return resolve(resultSearchData);
+      }
+
+      return resolve({ success: true, data: resultSearchData.data });
+    } catch (err) {
+      return resolve({ success: false, data: err });
+    }
+  });
+};
