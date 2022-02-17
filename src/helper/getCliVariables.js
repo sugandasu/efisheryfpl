9c28f32fd6f1325ae6dd3ddcb9e2ccbd7e5ff4b4
Arif Suganda <arifsuganda.su@gmail.com>
2022-02-17 16:39:37 +0800

    feat(helper): create getCliVariables
    Create getCliVariables to save transformation and data used in cli
    
    Feat

---
+++ src/helper/getCliVariables.js
@@ -0,0 +1,23 @@
+module.exports.columnList = [
+  "uuid",
+  "komoditas",
+  "area_provinsi",
+  "area_kota",
+  "size",
+  "harga",
+  "tanggal",
+  "timestamp",
+];
+
+module.exports.sheetList = [
+  "uuid",
+  "komoditas",
+  "area_provinsi",
+  "area_kota",
+  "size",
+  "price",
+  "tgl_parsed",
+  "timestamp",
+];
+
+module.exports.cliName = "efisheryfpl";
